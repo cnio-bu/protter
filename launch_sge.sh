31d9b451baec1235e887fd90c79cd9cfeea3ce32
Tomás Di Domenico <tdido@tdido.com.ar>
2019-01-08 10:44:31 +0100

    Removed unused SGE files.

--- launch_sge.sh
+++
@@ -1,8 +0,0 @@
-read -p "Is the conda environment activated? " -n 1 -r
-echo  
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    LOGDIR=log/cluster
-    mkdir -p $LOGDIR
-    snakemake -s Snakefile --drmaa-log-dir $LOGDIR --drmaa ' -cwd -pe cores {cluster.threads} -l h_vmem={cluster.mem}M -q {cluster.queue}' --cores 20 --cluster-config cluster_sge.yaml $1
-fi
