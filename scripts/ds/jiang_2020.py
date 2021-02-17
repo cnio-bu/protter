51b5a6b01abb146dc58713f679744cb801083d51
Thomas Walsh <twalsh@cnio.es>
2021-02-17 12:38:55 +0100

    Update grouping config

--- scripts/ds/jiang_2020.py
+++ scripts/ds/jiang_2020.py
@@ -4,10 +4,10 @@ import re
 def enhance_sample_metadata(ds_tab):
     enzymes = ["trypsin"] * len(ds_tab)
 
-    groups = list()
+    experiments = list()
     for _,row in ds_tab.iterrows():
-        match = re.match("^(?P<group>.+)_Fr[0-9]+$",row["sample"])
-        group = match.groupdict()["group"]
-        groups.append(group)
+        match = re.match("^(?P<experiment>.+)_Fr[0-9]+$",row["sample"])
+        experiment = match.groupdict()["experiment"]
+        experiments.append(experiment)
 
-    return ds_tab.assign(group=groups,enzyme=enzymes)
+    return ds_tab.assign(experiment=experiments,enzyme=enzymes)
